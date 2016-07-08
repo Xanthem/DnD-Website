33676b987c3ad7e64351cff7801b18e57223a311
Xanthem <tkkid@windstream.net>
2016-07-07 23:45:56 -0500

    Modularized DB Connections

---
+++ config/character_db_connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$DB_HOST = "127.0.0.1";
+$DB_NAME = "character_db";
+$DB_USER = "query";
+$DB_PASS = "D&D#DnD4Lyfe";
+
+$connect = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
--- signup.php
+++ signup.php
@@ -1,12 +1,6 @@
 <?php
 require_once "$_SERVER[DOCUMENT_ROOT]/config/basic_config.php";
-
-$DB_HOST = "127.0.0.1";
-$DB_NAME = "user_db";
-$DB_USER = "query";
-$DB_PASS = "D&D#DnD4Lyfe";
-
-$connect = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+require_once "$_SERVER[DOCUMENT_ROOT]/config/user_db_connect.php";
 
 ob_start();
 $msg = '';
