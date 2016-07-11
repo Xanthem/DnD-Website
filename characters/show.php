9d1c0a78591c2b248da29f62cdc1d7d42f83938c
Xanthem <tkkid@windstream.net>
2016-07-11 13:20:20 -0500

    Added Name for individual Character pages

--- characters/show.php
+++ characters/show.php
@@ -7,9 +7,10 @@ $sql = "SELECT *
         WHERE char_id = '$_GET[id]'";
 
 $result = mysqli_query($connect, $sql);
+$row = mysqli_fetch_array($result);
 $connect->close();
 ?>
 
 <body>
-<h1>Details for </h1>
+<h1>Details for <?php echo $row["char_name"]; ?></h1>
 </body>
